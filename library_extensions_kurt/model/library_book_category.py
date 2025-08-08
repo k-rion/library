6a5b42c60fc9f417fa1c2ee640b445afb630814b
DESKTOP-GBTV7E5\kurta <ritsushigeo41@gmail.com>
2025-08-08 19:13:32 +0800

    initial py library

---
+++ library_extensions_kurt/model/library_book_category.py
@@ -0,0 +1,8 @@
+from odoo import models, fields
+
+class LibraryBookCategory(models, Model):
+    _name = 'library.book.category'
+    _description = 'Library Book Category'
+    
+
+    name = fields.Char(string='Category Name', required=True , unique=True)
